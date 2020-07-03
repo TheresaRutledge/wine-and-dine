1c13b04680ad80b50198e73166f0532393b56abd
Lizette Leon <lizetteleon@Lizettes-Air.attlocal.net>
2020-07-02 20:51:34 -0700

    adding basic js and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,19 @@ const buttonHandler = (event) =>{
 }
 
 submitBtn.addEventListener('submit',buttonHandler);
+
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
+     
+
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
