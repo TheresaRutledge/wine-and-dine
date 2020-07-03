1cfceb5d9ed17ce02ce6100be254bd62a98e4f44
Theresa Rutledge <t.roserutledge@gmail.com>
2020-07-02 22:06:56 -0700

    api call made and response info saved into variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,42 @@
-let submitBtn = document.querySelector('.btn');
-
-const buttonHandler = (event) =>{
-    preventDefault();
-}
-
-submitBtn.addEventListener('submit',buttonHandler);
-
-
-$(document).ready(function(){
-  $('select').formSelect();
-});
-     
+const spoonApiKey = '5bb17ff8de4a4ac48777208734e43797';
+const submitBtn = document.querySelector('.btn');
+let pairedWines=[];
+let pairedText;
+  
 
 
 (function($){
   $(function(){
-
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+//functions to call when submit clicked
+const buttonHandler = (event) =>{
+    event.preventDefault();
+    foodInput = 'burger'//Need selector here when html is done
+    getWinePairing();
+    //add other function calls here
+}
+
+
+//fetches wine pairing and text
+const getWinePairing = () => {
+    fetch(`https://api.spoonacular.com/food/wine/pairing?food=${foodInput}&apiKey=${spoonApiKey}`)
+        .then(function (response) {
+            response.json()
+            .then(function (data) {
+                console.log(data);
+                //wine varietals that pair with input
+                pairedWines=data.pairedWines;
+                //text supplied for pairing
+                pairedText = data.pairedText;
+            })
+        })
+}
+
+
+//submit button listener
+submitBtn.addEventListener('click',buttonHandler);
