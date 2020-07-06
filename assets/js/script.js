8499e287794ab88a44adc488ee8523a40a397a6b
osborn roxas <osbornroxas02@gmail.com>
2020-07-05 19:20:30 -0700

    commitng saved

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,28 +20,6 @@ $(document).ready(function () {
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-
-//PUSH PIN
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.pushpin');
-  var instances = M.Pushpin.init(elems, options);
-});
-
-// Or with jQuery
-$(document).ready(function () {
-  $('.pushpin').pushpin();
-});
-
-$('.pushpin-demo-nav').each(function () {
-  var $this = $(this);
-  var $target = $('#' + $(this).attr('data-target'));
-  $this.pushpin({
-    top: $target.offset().top,
-    bottom: $target.offset().top + $target.outerHeight() - $this.height()
-  });
-});
-
-
 //SEARCH BAR
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
