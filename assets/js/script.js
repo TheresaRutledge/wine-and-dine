47dc343a926a9ef3ce01e0ab73a9144846dc0eb6
Lizette Leon <lizette.leon14@berkeley.edu>
2020-07-07 18:30:25 -0700

    made recommendations and previous searches responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 const spoonApiKey = '5bb17ff8de4a4ac48777208734e43797';
 const submitBtn = document.querySelector('.btn');
-let pairedWines=[];
+let pairedWines = [];
 let pairedText;
 let photoUrls = [];
 let winePhotos;
-  
 
 
-(function($){
-  $(function(){
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
 
-  }); // end of document ready
+(function ($) {
+    $(function () {
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 //functions to call when submit clicked
-const buttonHandler = (event) =>{
+const buttonHandler = (event) => {
     event.preventDefault();
     foodInput = 'burger'//Need selector here when html is done
     getWinePairing();
@@ -32,33 +32,33 @@ const getWinePairing = () => {
     fetch(`https://api.spoonacular.com/food/wine/pairing?food=${foodInput}&apiKey=${spoonApiKey}`)
         .then(function (response) {
             response.json()
-            .then(function (data) {
-                console.log(data);
-                //wine varietals that pair with input
-                pairedWines=data.pairedWines;
-                //text supplied for pairing
-                pairedText = data.pairedText;
-            })
+                .then(function (data) {
+                    console.log(data);
+                    //wine varietals that pair with input
+                    pairedWines = data.pairedWines;
+                    //text supplied for pairing
+                    pairedText = data.pairedText;
+                })
         })
 }
 //fetches 10 wine images and stores to photoUrls
-function getWinePhotos () {
-  fetch(
-    'https://api.unsplash.com/search/photos?query=wine&' +
-      'client_id=BSXJED6UhyH4DSPVkqo8B2ThVz-Hyq083g1E7AhrR1k'
-  )
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (response) {
-      console.log(response)
-      winePhotos = response.results
-      winePhotos.forEach(photo => {
-        photoUrls.push(photo.urls.regular)
-      })
-    })
-  console.log(photoUrls) //For test only
+function getWinePhotos() {
+    fetch(
+        'https://api.unsplash.com/search/photos?query=wine&' +
+        'client_id=BSXJED6UhyH4DSPVkqo8B2ThVz-Hyq083g1E7AhrR1k'
+    )
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (response) {
+            console.log(response)
+            winePhotos = response.results
+            winePhotos.forEach(photo => {
+                photoUrls.push(photo.urls.regular)
+            })
+        })
+    console.log(photoUrls) //For test only
 }
 
 //submit button listener
-submitBtn.addEventListener('click',buttonHandler);
+submitBtn.addEventListener('click', buttonHandler);
